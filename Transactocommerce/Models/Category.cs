747e9749fd0793e26101fedb90df50299aab4e08
Rami Hikmat <ramihikmat10@gmail.com>
2020-07-22 00:15:33 +1000

    Initial Models

---
+++ Transactocommerce/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transactocommerce.Models
+{
+    public class Category
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [NotNull]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Transactocommerce/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transactocommerce.Models
+{
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [NotNull]
+        [Required]
+        public string Name { get; set; }
+
+        [NotNull]
+        public string Description { get; set; }
+
+        [NotNull]
+        [Required]
+        [Column(TypeName = "money")] // Postgresql data type
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [NotNull]
+        [Required]
+        public string Manufacturer { get; set; }
+
+        [ForeignKey("Category")]
+        [NotNull]
+        public string CategoryId { get; set; }
+
+        public int Stock { get; set; } = 0;
+    }
+}
